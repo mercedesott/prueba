4c8a16f346f7210f4d6fe149b40d4d09bc6aa46b
Mercedes Ott <mermariaott@yahoo.com>
2012-11-17 11:15:42 -0200

    prueba

---
+++ PruebaCake/app/Model/User.php
@@ -0,0 +1,21 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * User Model
+ *
+ */
+class User extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'username';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+}
---
+++ PruebaCake/app/View/Posts/index.ctp
@@ -0,0 +1,6 @@
+<?php echo $this->Form->create('Post'); ?>
+<?php echo $this->Form->input('title', array('class'=>'NOENTIENDONADAESTOPARECETREMENDAPORONGA')); ?>
+<?php echo $this->Form->end(); ?>
+<h1>
+	<?php echo $posts; ?>
+</h1>
